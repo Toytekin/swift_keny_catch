340b6c02b6c407aaadbaf1c5d8d16dec08e345ab
Toytekin <btoytekin80@gmail.com>
2025-01-16 20:45:19 +0300

    Initial Commit

---
+++ funnycatch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  funnycatch
+//
+//  Created by İbrahim Toytekin on 16.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
